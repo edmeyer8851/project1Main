bdf8464887c8704771c6786c887615c65498c881
wendy <mizukilee@gmail.com>
2022-12-05 17:39:02 -0500

    added more details

--- index.js
+++ index.js
@@ -22,11 +22,29 @@ function displayDetails(e) {
             let name = document.createElement('h1')
             name.textContent = killer.name
             detailsContainer.appendChild(name)
-            // 
+            // countries active // 
+            let countryLabel = document.createElement('h1')
+            countryLabel.textContent = `Country: `
             let country = document.createElement('h1') 
-
+            country.textContent = killer.country
+            countryLabel.appendChild(country)
+            detailsContainer.appendChild(countryLabel)
+            // years active // 
+            let yearsActive = document.createElement('h1')
+            yearsActive.textContent = killer.yearsActive
+            detailsContainer.appendChild(yearsActive)
+            // # of victims // 
+            let victims = document.createElement('h1') 
+            victims.textContent = killer.numberVictims 
+            detailsContainer.appendChild(victims)
+            // description 
+            let description = document.createElement('h1')
+            description.textContent = killer.description
+            detailsContainer.appendChild(description)
+            // dislikes //
+            let dislikes = document.createElement('h1') 
+            dislikes.textContent = killer.dislikes
+            detailsContainer.appendChild(dislikes)
+            console.log(detailsContainer)
         })
-    
-    
-
 }
