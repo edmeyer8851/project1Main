b242cdd46f2d5d87f8e156baa98ffbed680f7eb5
wendy <mizukilee@gmail.com>
2022-12-05 16:59:23 -0500

    name to details

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const imagesContainer = document.getElementById('menu-bar')
+const detailsContainer = document.getElementById('details') 
 
 fetch('http://localhost:3000/serialKillers')
  .then(response => response.json())
  .then(killers => {
     killers.forEach(killer => {
         let img = document.createElement('img')
+        img.addEventListener('click', displayDetails)
         img.src = killer.image
         img.id = killer.id
         imagesContainer.appendChild(img)
     })
  })
 
- // this is my test commit #2
- // another comment
+function displayDetails(e) { 
+    fetch(`http://localhost:3000/serialKillers/${e.target.id}`) 
+        .then(response => response.json()) 
+        .then(killer => {
+            // name 
+            detailsContainer.innerHTML = ''
+            let name = document.createElement('h1')
+            name.textContent = killer.name
+            detailsContainer.appendChild(name)
+            // 
+            let country = document.createElement('h1') 
+
+        })
+    
+    
+
+}
