e3ff4ad9509fe43655b7f8b52ad774b233119ff9
Ed Meyer <edmeyer8851@gmail.com>
2022-12-05 17:45:33 -0500

    added some more elements to the detail section

--- index.js
+++ index.js
@@ -17,16 +17,33 @@ function displayDetails(e) {
     fetch(`http://localhost:3000/serialKillers/${e.target.id}`) 
         .then(response => response.json()) 
         .then(killer => {
-            // name 
-            detailsContainer.innerHTML = ''
-            let name = document.createElement('h1')
+            // name
+            detailsContainer.innerHTML = ``
+            let name = document.createElement(`h1`)
             name.textContent = killer.name
             detailsContainer.appendChild(name)
-            // 
-            let country = document.createElement('h1') 
-
-
-
+            // countries active //
+            let country = document.createElement(`h1`)
+            country.textContent = killer.country
+            countryLabel.appendChild(country)
+            detailsContainer.appendChild(countryLabel)
+            // years active //
+            let yearsActive = document.createElement(`h1`)
+            yearsActive.textContent = killer.yearsActive
+            detailsContainer.appendChild(yearsActive)
+            // # of victims //
+            let victims = document.createElement(`h1`)
+            victims.textContent = killer.numberVictims
+            detailsContainer.appendChild(victims)
+            // description
+            let description = document.createElement(`h1`)
+            description.textContent = killer.description
+            detailsContainer.appendChild(description)
+            // dislikes //
+            let dislikes = document.createElement(`h1`)
+            dislikes.textContent = killer.dislikes
+            detailsContainer.appendChild(dislikes)
+            console.log(detailsContainer)
 
             // dislike button
             let dislikeButton = document.createElement('button')
